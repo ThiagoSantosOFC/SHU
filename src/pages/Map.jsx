2599b0d0cdb86229c0b3d46980c02ed96f7e1354
Thiago <74683757+ThiagoSantosOFC@users.noreply.github.com>
2023-07-07 16:39:37 +0100

    a

--- src/pages/Map.jsx
+++ src/pages/Map.jsx
@@ -2,17 +2,23 @@ import React from 'react';
 
 function Map() {
   return (
-    <div className="min-h-full items-center columns-1 justify-center py-16 px-4 sm:px-6 lg:px-8 ">
-      <br />
-
-      <div className="relative p-40 max-w-full">
+    <div className="relative p-40 max-w-full">
+      <style>
+        {`.embed-container {position: relative; padding: 40px; height: 400; max-width: 100%;} 
+        .embed-container iframe, .embed-container object, .embed-container iframe{position: absolute; top: 0; left: 0; width: 100%; height: 100%;} 
+        small{position: absolute; z-index: 40; bottom: 0; margin-bottom: -15px;}`}
+      </style>
+      <div className="embed-container">
         <iframe
-          className="relative m-auto w-600 h-300"
-          style={{ widt3: '75%' }}
           title="Cedat"
-          src="//www.arcgis.com/apps/Embed/index.html?webmap=8b596e823b454226aa846a2d5e57fccf&extent=12.9481,43.1137,12.9705,43.122&zoom=true&previewImage=false&scale=true&disable_scroll=true&theme=light"
+          src="//www.arcgis.com/apps/Embed/index.html?webmap=8b596e823b454226aa846a2d5e57fccf&extent=11.6038,42.5492,14.4657,43.6184&zoom=true&previewImage=false&scale=true&details=true&legendlayers=true&active_panel=details&disable_scroll=false&theme=light"
+          width="300"
+          height="500"
+          frameBorder="0"
+          scrolling="no"
+          marginHeight="0"
+          marginWidth="0"
         ></iframe>
-        <small className="absolute z-40 bottom-0 mb-[15px]"></small>
       </div>
     </div>
   );
