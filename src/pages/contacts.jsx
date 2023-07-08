c669124a1ec6fccaa7be2e78d9719c6c62b4caf0
Thiago <74683757+ThiagoSantosOFC@users.noreply.github.com>
2023-07-08 18:25:58 +0100

    funfionca

--- src/pages/contacts.jsx
+++ src/pages/contacts.jsx
@@ -12,19 +12,19 @@ export default function contactos() {
             <div className="w-full">
               <h1 className="mt-40 mb-4 text-3xl md:text-5xl font-bold">Contattaci</h1>
               <p className="mb-4 text-sm md:text-base text-gray-700">
-                Il CEDAT, Centro di Documentazione dell’Acqua e della Terra, è
-                situato nella splendida cornice di Palazzo Camilli. L’ambizione
+                Il CEDAT, Centro di Documentazione dell&apos;Acqua e della Terra, è
+                situato nella splendida cornice di Palazzo Camilli. L&apos;ambizione
                 è quella di realizzare un centro studi che diventi un punto di
-                riferimento relativamente all'argomento "Acqua". <br />
+                riferimento relativamente all&apos;argomento &ldquo;Acqua&rdquo;. <br />
                 <br />
-                Il CEDAT, Centro di Documentazione dell'Acqua e della Terra, si
-                articola su due livelli all'interno di Palazzo Camilli,
-                quest'ultimo situato nel centro storico.
+                Il CEDAT, Centro di Documentazione dell&apos;Acqua e della Terra, si
+                articola su due livelli all&apos;interno di Palazzo Camilli,
+                quest&apos;ultimo situato nel centro storico.
                 <br />
                 <br />
                 Il primo livello, sviluppato al piano terra e nelle due stanze
-                dell'edificio, rappresenta i due elementi caratterizzanti
-                Nocera: l'acqua, con le tre sorgenti principali Angelica,
+                dell&apos;edificio, rappresenta i due elementi caratterizzanti
+                Nocera: l&apos;acqua, con le tre sorgenti principali Angelica,
                 Cacciatore e Flaminia, e la terra, con i suoi differenti
                 utilizzi, a fini terapeutici e nella lavorazione del Calcare
                 Bianco.
@@ -33,13 +33,13 @@ export default function contactos() {
                 Il secondo livello, allestito al primo piano, è sede di una
                 mostra permanente cui verrà diversificata e rinnovata nel tempo:
                 la storia del Bagno di Nocera, la pubblicità realizzata da
-                Felice Bisleri, oltre alla presentazione dei vari punti d'acqua
+                Felice Bisleri, oltre alla presentazione dei vari punti d&apos;acqua
                 presenti sul territorio.
                 <br />
                 <br />
                 Due sale per conferenze dotate dei necessari supporti
                 informatici, infine, al secondo piano della struttura, nonché
-                sede dell'associazione culturale "L'Arengo".
+                sede dell&apos;associazione culturale &ldquo;L&apos;Arengo&rdquo;.
               </p>
               <p className="mt-2 mb-2 text-sm md:text-base text-gray-700">
                 Tel: +39 0742 834011 Fax: +39 0742 812133
--- src/pages/knowmore.jsx
+++ src/pages/knowmore.jsx
@@ -16,7 +16,7 @@ export default function Knowmore() {
 
               <p className="mb-4 text-sm md:text-base text-gray-700">
                 Water Heritage Nocera Umbra è un progetto che si compone di{" "}
-                <b>due parti, una digitale e una analogica</b>, per dare un'
+                <b>due parti, una digitale e una analogica</b>, per dare un&apos;
                 <b>esperienza completa e autonoma al visitatore</b> che vuole
                 partire alla scoperta delle acque di Nocera Umbra.
               </p>
@@ -24,9 +24,9 @@ export default function Knowmore() {
               <h2 className="text-lg mt-4">Digitalizzazione delle acque di Nocera Umbra</h2>
               <br/>
               <p className="mb-4 text-sm md:text-base text-gray-700">
-                Nell'ambito del progetto, è stata{" "}
-                <b>sviluppata una web app che offre accesso diretto e facile a una mappa che elenca con precisione le coordinate dei 305 luoghi identificati dal CEDAT</b>. I luoghi sono suddivisi per categoria e, al momento della selezione da parte dell'utente, un popup mostra le informazioni più rilevanti sul luogo scelto, come le coordinate geografiche, il tipo di sorgente, il materiale, le dimensioni, lo stato di funzionamento, le condizioni di conservazione e altre informazioni varie. <br/>
-                Tutti <b>i dati inseriti sono stati raccolti dal CEDAT</b> nel corso di due anni e si riferiscono all'anno 2020. Lo scopo di questa ricerca e mappatura dei luoghi legati all'acqua è di ottenere una conoscenza approfondita sulle fonti d'acqua di Nocera Umbra e facilitare uno studio sul loro stato di conservazione.
+                Nell&apos;ambito del progetto, è stata{" "}
+                <b>sviluppata una web app che offre accesso diretto e facile a una mappa che elenca con precisione le coordinate dei 305 luoghi identificati dal CEDAT</b>. I luoghi sono suddivisi per categoria e, al momento della selezione da parte dell&apos;utente, un popup mostra le informazioni più rilevanti sul luogo scelto, come le coordinate geografiche, il tipo di sorgente, il materiale, le dimensioni, lo stato di funzionamento, le condizioni di conservazione e altre informazioni varie. <br/>
+                Tutti <b>i dati inseriti sono stati raccolti dal CEDAT</b> nel corso di due anni e si riferiscono all&apos;anno 2020. Lo scopo di questa ricerca e mappatura dei luoghi legati all&apos;acqua è di ottenere una conoscenza approfondita sulle fonti d&apos;acqua di Nocera Umbra e facilitare uno studio sul loro stato di conservazione.
               </p>
 
               <h2 className="text-lg mt-4">
@@ -36,8 +36,8 @@ export default function Knowmore() {
               <p className="mb-4 text-sm md:text-base text-gray-700">
                 La seconda parte del progetto riguarda la{" "}
                 <b>progettazione grafica di schede informative da posizionare nei luoghi di interesse</b> indicati sulla mappa. I visitatori che desiderano esplorare di persona un determinato luogo potranno{" "}
-                <b>accedere, tramite smartphone, alle informazioni registrate in doppia</b> lingua italiana e inglese semplicemente <b>inquadrando un codice QR</b> o avvicinando il dispositivo a <b>un tag NFC</b>. La tecnologia NFC consente la condivisione di contenuti senza la necessità di installare un'app dedicata. È facile da programmare e richiede solo uno smartphone compatibile. <br/>
-                L'introduzione degli elementi analogici mira a <b>fornire ai visitatori</b>, che sono arrivati in un luogo specifico utilizzando la mappa digitale o che vi sono casualmente arrivati dopo una passeggiata, la stessa <b>accessibilità alle informazioni</b>. In questo modo, coloro interessati a proseguire il loro percorso possono venire a conoscenza della mappa digitale o continuare a consultarla per scoprire tutti gli altri luoghi d'interesse.
+                <b>accedere, tramite smartphone, alle informazioni registrate in doppia</b> lingua italiana e inglese semplicemente <b>inquadrando un codice QR</b> o avvicinando il dispositivo a <b>un tag NFC</b>. La tecnologia NFC consente la condivisione di contenuti senza la necessità di installare un&apos;app dedicata. È facile da programmare e richiede solo uno smartphone compatibile. <br/>
+                L&apos;introduzione degli elementi analogici mira a <b>fornire ai visitatori</b>, che sono arrivati in un luogo specifico utilizzando la mappa digitale o che vi sono casualmente arrivati dopo una passeggiata, la stessa <b>accessibilità alle informazioni</b>. In questo modo, coloro interessati a proseguire il loro percorso possono venire a conoscenza della mappa digitale o continuare a consultarla per scoprire tutti gli altri luoghi d&apos;interesse.
               </p>
             </div>
           </div>
