900647f3c2bc6e63cd324332a7204a319cba30ea
VascomDuarte <100935953+VascomDuarte@users.noreply.github.com>
2023-07-08 15:51:39 +0100

    Merged

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import App from 'next/app';
-
 import NavBar from '../components/NavBar';
 import SelectLang from '../components/SelectLang';
 import '../styles/globals.css';
